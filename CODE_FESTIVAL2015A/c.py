d82b67b0a102722059e205c4e01916a912e124cb
ryosukee <tmcit.miyazaki@gmail.com>
2015-09-27 00:09:30 +0900

    [fix] comparison expression

--- CODE_FESTIVAL2015A/c.py
+++ CODE_FESTIVAL2015A/c.py
@@ -13,18 +13,17 @@ for i in range(num):
     a, b = input().split()
     a = int(a)
     b = int(b)
-    difference = a - b
     
-    bisect.insort(differences, difference)
+    bisect.insort(differences, a - b)
     total_a += a
-    if total_a < dead:
+    if total_a <= dead:
         count += 1
     else:
         # remove max difference item
         total_a -= differences.pop(-1)
     total_b += b
 
-if total_b > dead:
+if total_b >= dead:
     print(-1)
 else:
     print(num - count)
